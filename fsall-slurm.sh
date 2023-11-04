354b79fd74f1cf64ee879fa6b59557b339ad4d60
AWS ParallelCluster user <ec2-user@ip-10-0-12-223.ap-southeast-1.compute.internal>
2023-11-04 15:07:31 +0000

    syntax

--- fsall-slurm.sh
+++ fsall-slurm.sh
@@ -17,5 +17,5 @@ import DataProcessingTools as DPT; \
 lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1); \
 lfall.save(); \
 hfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
-hfall.save();
+hfall.save();"
 aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:947405335717:awsnotify --message "FSJobDone"
